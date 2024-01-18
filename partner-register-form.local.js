80a096c068ced42fab3d59751dbe554577bc4f24
Nitzan Savion <59604278+nizans@users.noreply.github.com>
2024-01-18 14:21:39 +0200

    env

--- partner-register-form.local.js
+++ partner-register-form.local.js
@@ -1,8 +1,5 @@
 function initializeForm() {
-  // Multistep form logic
-  // Dev API: https://909c-89-205-135-183.ngrok-free.app/v1
-  // Preview API: https://prodrops-api-production.up.railway.app/v1
-  const PD_REGISTER_API = "https://prodrops-dev.up.railway.app/v1";
+  const PD_REGISTER_API = "https://60b1-192-115-72-58.ngrok-free.app";
   let currentStep = 1;
   const typeInput = document.querySelector("select[name=type]");
   const warningModal1 = document.querySelector("[data-form-warning-1]");
@@ -15,7 +12,7 @@ function initializeForm() {
     "#registrationFormStep3Wrapper .input-error"
   );
   const submitButton = document.querySelector("#partner-form-submit");
-  const api = PD_REGISTER_API;
+  const api = PD_REGISTER_API + "/v1";
   const successModal = document.querySelector("#form-success-wrapper");
   let isSubmitting = false;
   var minAge13 = ["US"];
